214392694583f6879033ece623746fa6a095e6a9
Bhumika Sahai <sahaibhumika04@gmail.com>
2025-09-16 16:21:04 +0530

    update

---
+++ 904.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int i=0;
+        int j=0;
+        int size=0;
+        while(j<fruits.length){
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            while(map.size()>2){
+                map.put(fruits[i], map.get(fruits[i]) - 1);
+                if (map.get(fruits[i]) == 0) {
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }  
+            size = Math.max(size,j-i+1);
+            j++;     
+        }
+        return size;
+    }
+}
