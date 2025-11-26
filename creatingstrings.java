9b40f1b1c44531a111d3744799f03ee3987e39bd
Bhumika Sahai <sahaibhumika04@gmail.com>
2025-11-26 10:09:02 +0530

    update

--- creatingstrings.java
+++ creatingstrings.java
@@ -1,8 +1,38 @@
 import java.util.*;
 public class creatingstrings {
-    boolean used;
-    static char arr[];
-    static StringBuilder sb = new StringBuilder();
-    static List<String> result = new ArrayList<>(); 
-    public    
+    static ArrayList<String> result = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();       
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr); 
+        solve(arr, 0);
+        Collections.sort(result);
+        System.out.println(result.size());
+        for(String str : result){
+            System.out.println(str);
+        }
+    }
+    public static void solve(char[] arr, int idx) {
+        if(idx == arr.length){
+            result.add(new String(arr));
+            return;
+        }
+        HashSet<Character> set = new HashSet<>();      
+        for(int i = idx; i < arr.length; i++){
+            if(set.contains(arr[i])) continue; 
+            set.add(arr[i]);
+            swap(arr, idx, i);
+            solve(arr, idx+1);
+            swap(arr, idx, i); 
+        }
+    }
+    public static void swap(char[] arr, int i, int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
+
+    
+
