30f0c074071f978d8b5da0dcd0bf8dd63b4471e0
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-02-12 12:13:15 +0530

    update commit

--- longestflightroute.java
+++ longestflightroute.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class longestflioghtroute {
+public class longestflightroute {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
