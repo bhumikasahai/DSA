ea25a8a2b03d47e8b1ff45858e832bc1f75d31ba
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-02-19 12:17:22 +0530

    update

---
+++ jzzhuandcities.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class jzzhuandcities {
+    static class Graph{
+        int V;
+        ArrayList<ArrayList<Pair>> adj;
+        Graph(int V){
+            this.V = V;
+            adj = new ArrayList<>();
+            for(int i=0;i<V;i++){
+                adj.add(new ArrayList<>());
+            }
+        }
+    }
+    static class Pair{
+        int d;
+        int n;
+        Pair(int d, int n){
+            this.d = d;
+            this.n = n;
+        }
+    }
+    public static solve(ArrayList<ArrayList<Pair>> adj = new ArrayList<>()){
+        int dist[] = new int
+    }
+
+
+
+
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+    }
+}
