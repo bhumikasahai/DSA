c68143663fd80b1fc50a079e5e7b993ca4b42beb
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-02-24 21:55:18 +0530

    update

--- roadconstruction.java
+++ roadconstruction.java
@@ -1,67 +1,50 @@
 import java.util.*;
+import java.io.*; 
 public class roadconstruction {
     static int component;
-    static class Edge{
-        int w;
-        int u;
-        int v;
-        Edge(int w, int u, int v){
-            this.w = w;
-            this.u = u;
-            this.v = v;
-        }
-    }
-    public static int find(int i, int parent[]){
-        if(i==parent[i]){
+    static int max_size = 1; 
+    public static int find(int i, int parent[]) {
+        if (i == parent[i]) {
             return i;
         }
-        return parent[i] = find(parent[i],parent);
+        return parent[i] = find(parent[i], parent);
     }
-    public static boolean union(int u, int v, int parent[], int size[]){
-        int u_parent = find(u,parent);
-        int v_parent = find(v,parent);
-        if(u_parent == v_parent){
-            return false;
+    public static void union(int u, int v, int parent[], int size[]) {
+        int u_parent = find(u, parent);
+        int v_parent = find(v, parent);
+        if (u_parent != v_parent) {
+            if (size[u_parent] < size[v_parent]) {
+                parent[u_parent] = v_parent;
+                size[v_parent] += size[u_parent];
+                max_size = Math.max(max_size, size[v_parent]);
+            } else {
+                parent[v_parent] = u_parent;
+                size[u_parent] += size[v_parent];
+                max_size = Math.max(max_size, size[u_parent]);
+            }
+            component--; 
         }
-        if(size[u_parent]<size[v_parent]){
-            parent[u_parent] = v_parent;
-            size[v_parent] += size[u_parent];
-        }else{
-            parent[v_parent] = u_parent;
-            size[u_parent] += size[v_parent];
-        }
-        component--;
-        return true;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt();
-        int E = sc.nextInt();
-        List<Edge> edges = new ArrayList<>();
-        for(int i = 0; i < E; i++) {
-            int u = sc.nextInt(); 
-            int v = sc.nextInt();
-            int w = sc.nextInt();
-            edges.add(new Edge(w, u, v));
-        }
-        edges.sort(Comparator.comparingInt(e -> e.w));
-        int parent[] = new int[V+1];
-        int size[] = new int[V+1];
-        for(int i=1;i<=V;i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int parent[] = new int[V + 1];
+        int size[] = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
             parent[i] = i;
             size[i] = 1;
         }
         component = V;
-        long totalcost = 0;
-        for(Edge e : edges) {
-            if(union(e.u, e.v, parent, size)) {
-                totalcost += e.w;
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            union(u, v, parent, size);
+            sb.append(component).append(" ").append(max_size).append("\n");
         }
-        if(component == 1) {
-            System.out.println(totalcost);
-        } else {
-            System.out.println("IMPOSSIBLE");
-        }
+        System.out.print(sb);
     }
 }
--- roadreparartion.java
+++ roadreparartion.java
@@ -1,64 +1,71 @@
 import java.util.*;
+import java.io.*; 
 public class roadreparartion {
     static int component;
-    static class Edge{
+    static class Edge {
         int w;
         int u;
         int v;
-        Edge(int w, int u, int v){
+        Edge(int w, int u, int v) {
             this.w = w;
             this.u = u;
             this.v = v;
         }
     }
-    public static int find(int i, int parent[]){
-        if(i==parent[i]){
+    public static int find(int i, int parent[]) {
+        if (i == parent[i]) {
             return i;
         }
-        return parent[i] = find(parent[i],parent);
+        return parent[i] = find(parent[i], parent);
     }
-    public static boolean union(int u, int v, int parent[], int size[]){
-        int u_parent = find(u,parent);
-        int v_parent = find(v,parent);
-        if(u_parent == v_parent){
+    public static boolean union(int u, int v, int parent[], int size[]) {
+        int u_parent = find(u, parent);
+        int v_parent = find(v, parent);
+        if (u_parent == v_parent) {
             return false;
         }
-        if(size[u_parent]<size[v_parent]){
+        if (size[u_parent] < size[v_parent]) {
             parent[u_parent] = v_parent;
             size[v_parent] += size[u_parent];
-        }else{
+        } else {
             parent[v_parent] = u_parent;
             size[u_parent] += size[v_parent];
         }
         component--;
         return true;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt();
-        int E = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
+        if (line1 == null) return;
+        StringTokenizer st = new StringTokenizer(line1);
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
         List<Edge> edges = new ArrayList<>();
-        for(int i = 0; i < E; i++) {
-            int u = sc.nextInt(); 
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+        for (int i = 0; i < E; i++) {
+            String line = br.readLine();
+            if (line == null) break;
+            st = new StringTokenizer(line);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
             edges.add(new Edge(w, u, v));
         }
         edges.sort(Comparator.comparingInt(e -> e.w));
-        int parent[] = new int[V+1];
-        int size[] = new int[V+1];
-        for(int i=1;i<=V;i++){
+        int parent[] = new int[V + 1];
+        int size[] = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
             parent[i] = i;
             size[i] = 1;
         }
         component = V;
         long totalcost = 0;
-        for(Edge e : edges) {
-            if(union(e.u, e.v, parent, size)) {
+        for (Edge e : edges) {
+            if (union(e.u, e.v, parent, size)) {
                 totalcost += e.w;
             }
         }
-        if(component == 1) {
+        if (component == 1) {
             System.out.println(totalcost);
         } else {
             System.out.println("IMPOSSIBLE");
