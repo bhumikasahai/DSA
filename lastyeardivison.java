cbec64e2dfcd33a5175bfc6b42d46d04578e68bd
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-03-15 17:43:04 +0530

    update

---
+++ lastyeardivison.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class lastyeardivison{
+    static String target = "2020";
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = 8;
+            String s = sc.next();
+            char arr[] = new char[n];
+            
+        }
+    }
+    public static boolean solve(){
+
+    }
+}
