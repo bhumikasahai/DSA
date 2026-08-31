d6bd56dd237376b4d301bf7f2a48d1cd0bc59715
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-09-01 00:44:30 +0530

    update

--- Basic-DSA-PI/fibonacci.java
+++ Basic-DSA-PI/fibonacci.java
@@ -3,17 +3,33 @@ public class fibonacci{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        solve(n);
-    }
-    public static void solve(int n){
-        int a = 0;
-        int b = 1;
-        for(int i=1;i<n;i++){
-            System.out.print(a + " ");
-            int next = a+b;
-            a = b;
-            b=next;
+        //solve(n);
+        for(int i=0;i<n;i++){
+            System.out.print(solve(i)+" ");
         }
-        System.out.println();
+    }
+
+    //ITERATIVE
+    //public static void solve(int n){
+    //    int a = 0;
+    //    int b = 1;
+    //    for(int i=1;i<n;i++){
+    //        System.out.print(a + " ");
+    //        int next = a+b;
+    //        a = b;
+    //        b=next;
+    //    }
+    //    System.out.println();
+    //}
+
+    //RECURSIVE
+    public static int solve(int n){
+        if(n==0){
+            return 0;    
+        }
+        if(n==1){
+            return 1;
+        }
+        return solve(n-1)+solve(n-2);
     }
 }
