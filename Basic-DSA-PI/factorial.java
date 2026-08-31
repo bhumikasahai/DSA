0fd6b9d134c6b7461065f020533a4a98b5a77534
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-09-01 01:28:21 +0530

    update

---
+++ Basic-DSA-PI/factorial.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solve(n));
+    }   
+    
+    //BRUTE
+    //public static int solve(int n){
+    //    if(n==0 || n==1){
+    //        return 1;
+    //    }
+    //    int fact = 1;
+    //    for(int i=2;i<=n;i++){
+    //        fact = fact*i;
+    //    }
+    //    return fact;
+    //}
+
+    //OPTIMAL
+    public static int solve(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*solve(n-1);
+    }
+}
