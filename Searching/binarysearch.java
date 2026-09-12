e9a2389d1f1ef70174b9439f88539cdbe52cbdfc
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-09-12 14:54:52 +0530

    update commit

--- Searching/binarysearch.java
+++ Searching/binarysearch.java
@@ -1,3 +1,30 @@
+import java.util.*;
 public class binarysearch {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        boolean found = false;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]==target){
+                System.out.println(mid);
+                found = true;
+                break;
+            }else if(arr[mid]<target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        if(found==false){
+            System.out.println(-1);
+        }
+    }
 }
