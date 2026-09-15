743f391a4f7456947e679554a7c6094adc9e8eb4
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-09-15 20:14:49 +0530

    update commit

---
+++ LinkedList/findmiddle.java
@@ -0,0 +1,28 @@
+public class findmiddle {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static Node middle(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+        Node mid = middle(head);
+        System.out.println(mid.data);
+    }
+}
