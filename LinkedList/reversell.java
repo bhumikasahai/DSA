e130f54540cd0168dddb15fb7da9ca573047a0c2
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-09-15 20:04:59 +0530

    update

---
+++ LinkedList/reversell.java
@@ -0,0 +1,38 @@
+public class reversell {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        print(head);
+        head = reverse(head);
+        print(head);
+    }
+}
